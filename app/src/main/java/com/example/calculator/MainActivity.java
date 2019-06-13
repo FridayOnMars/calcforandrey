db8d38c3a4a19effbf5d92ed38c4c75b2b57bdd2
Falcon <euromarvel@gmail.com>
2019-06-13 18:29:09 +0400

    Fix operations buttons

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.calculator;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.pm.ActivityInfo;
@@ -14,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
     EditText number;   // поле для ввода числа
     Double operand = null;  // операнд операции
     String Operation = "="; // последняя операция
-    //TextView test;
+    private boolean clickable = false;
+    TextView test;
 
     double num1 = 0;
     int b=0;
@@ -26,10 +28,11 @@ public class MainActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         result =(TextView) findViewById(R.id.result);
         number =(EditText) findViewById(R.id.number);
-        //test = (TextView) findViewById(R.id.test);
+        test = (TextView) findViewById(R.id.test);
     }
     public void onNumberClick(View view){
         Button button = (Button) view;
+        clickable = false;
         number.append(button.getText());
         if(Operation.equals("=") && operand!=null){
             operand = null;
@@ -41,17 +44,22 @@ public class MainActivity extends AppCompatActivity {
         result.setText("");
         Operation = "=";
         num1=0;
+        b=0;
     }
     public void onOperationClick(View view){
         Button button = (Button) view;
         String op = button.getText().toString();
+        if (clickable == true) {
+            Operation = op;
+            number.setText(String.format("%s%s", number.getText().toString().substring(0, number.getText().toString().length() - 1), String.format("%s", op)));
+            return;
+        }
         String num = number.getText().toString();
         if(num.length()>0 && b==0) {
             num1 = Double.valueOf(num);
             b++;
             Operation = op;
-            number.append(op);
-            //test.append(num + op);
+            number.setText(String.format("%s%s", number.getText().toString(), op));
         }
         else if(num.length()>0 && b>0){
             String []a = num.split("\\" + Operation);
@@ -61,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
                 num1=0.0;
                 b=0;
                 number.setText("");
+                operand = 0.0;
                 Operation = op;
             }
             else{
@@ -69,8 +78,8 @@ public class MainActivity extends AppCompatActivity {
                 Operation = op;
             }
             number.append("\\" + op);
-            //test.append(num + op);
         }
+        clickable = true;
     }
     private void Operations(Double num, Double num1)
     {
