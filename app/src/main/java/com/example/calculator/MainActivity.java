9469a5a40abff4cd9ed16ddd0710c07a53eb9c37
Falcon <euromarvel@gmail.com>
2019-06-13 19:53:22 +0400

    Not end version, but this work!

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.pm.ActivityInfo;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,8 +17,8 @@ public class MainActivity extends AppCompatActivity {
     Double operand = null;  // операнд операции
     String Operation = "="; // последняя операция
     private boolean clickable = false;
+    private int clickpoint = 0;
     TextView test;
-
     double num1 = 0;
     int b=0;
 
@@ -33,9 +34,24 @@ public class MainActivity extends AppCompatActivity {
     public void onNumberClick(View view){
         Button button = (Button) view;
         clickable = false;
+        if(clickpoint!=2)
+        {
+            clickpoint = 1;
+        }
+        else
+        {
+            clickpoint = 3;
+        }
         number.append(button.getText());
         if(Operation.equals("=") && operand!=null){
             operand = null;
+        }
+    }
+    public void onPointClick(View view){
+        switch (clickpoint){
+            case 0: return;
+            case 1: number.setText(String.format("%s%s", number.getText().toString(), ".")); clickpoint=2; break;
+            
         }
     }
     public void onClearClick(View view){
@@ -45,10 +61,15 @@ public class MainActivity extends AppCompatActivity {
         Operation = "=";
         num1=0;
         b=0;
+        clickpoint = 0;
     }
     public void onOperationClick(View view){
         Button button = (Button) view;
         String op = button.getText().toString();
+        if(clickpoint == 2)
+        {
+            return;
+        }
         if (clickable == true) {
             Operation = op;
             number.setText(String.format("%s%s", number.getText().toString().substring(0, number.getText().toString().length() - 1), String.format("%s", op)));
@@ -71,6 +92,7 @@ public class MainActivity extends AppCompatActivity {
                 number.setText("");
                 operand = 0.0;
                 Operation = op;
+                clickable = false;
             }
             else{
                 Operations(Double.valueOf(num), num1);
@@ -84,26 +106,14 @@ public class MainActivity extends AppCompatActivity {
     private void Operations(Double num, Double num1)
     {
         switch (Operation) {
-            case "*":
-                operand = num1 * num;
-                break;
-            case "/":
-                operand = num1 / num;
-                break;
-            case "+":
-                operand = num1 + num;
-                break;
-            case "-":
-                operand = num1 - num;
-                break;
-            case "%":
-                operand = num1 % num;
-                break;
-            case "^":
-                operand = Math.pow(num1,num);
-                break;
+            case "*": operand = num1 * num; break;
+            case "/": operand = num1 / num; break;
+            case "+": operand = num1 + num; break;
+            case "-": operand = num1 - num; break;
+            case "%": operand = num1 % num; break;
+            case "^": operand = Math.pow(num1,num); break;
         }
-
+        clickpoint = 0;
         result.setText(operand.toString().replace('.',','));
     }
 }
